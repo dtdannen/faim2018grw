f27cbab70ffce2d31754cc4432b0d1eab2bf8ff7
Dustin Dannenhauer <dustin.td@gmail.com>
2018-03-08 16:44:38 -0500

    switched out old theme with Maks prior one

---
+++ docs/updateSite.sh
@@ -0,0 +1,4 @@
+
+echo "Copy the following line and run it at the cygwin prompt"
+scp -r *  makrob@makro.ink:/home/makrob/makro.ink/ijcai2017grw/
+
---
+++ mak/ijcai2018grw.tar/ijcai2018grw/open-perms.sh
@@ -0,0 +1,6 @@
+
+
+chmod 644 *.html *.php
+chmod 711 css fonts/ images/ js/ talks/ papers/
+chmod 644 css/* fonts/* images/* js/* talks/* papers/*
+
